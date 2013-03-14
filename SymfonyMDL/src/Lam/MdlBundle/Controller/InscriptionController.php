b94c818b95fd91ce335f19833b340d98d8d1b886
Beutin jeremy <je.beutin@laposte.net>
2013-03-14 17:25:20 +0100

    Modification des routes
    Creation des routes pour l'inscription

--- SymfonyMDL/src/Lam/MdlBundle/Controller/InscriptionController.php
+++ SymfonyMDL/src/Lam/MdlBundle/Controller/InscriptionController.php
@@ -7,8 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class InscriptionController extends Controller
 {
     
-    public function indexAction()
+    public function InscriptionInfoIndexAction()
     {
+        return $this->render('LamMdlBundle:Default:inscriptionForm.html.twig');
+    }
+    public function InscriptionSportIndexAction()
+    {
+        
         return $this->render('LamMdlBundle:Default:inscriptionForm.html.twig');
     }
 }
