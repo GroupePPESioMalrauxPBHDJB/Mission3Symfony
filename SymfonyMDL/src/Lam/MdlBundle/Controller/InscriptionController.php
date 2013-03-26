3b561fd5800d8c795c7637524a079e13d2e3ab3c
Beutin jeremy <je.beutin@laposte.net>
2013-03-26 17:31:48 +0100

    Modification Global 2
    controlleur, twig et type pour fonctionnement général.

--- SymfonyMDL/src/Lam/MdlBundle/Controller/InscriptionController.php
+++ SymfonyMDL/src/Lam/MdlBundle/Controller/InscriptionController.php
@@ -12,8 +12,11 @@ class InscriptionController extends Controller
     public function InscriptionInfoIndexAction($idFormation)
     {
         $em = $this->getDoctrine()->getEntityManager();
-        $laFormation=$em->getRepository('LamMdlBundle:formation')->find($idFormation);
+        $laFormation=$em->getRepository('LamMdlBundle:formation')->find($idFormation); 
         $laFormationInfo=$laFormation->getLaformationinformatique();
+        $dateStage=$laFormation->getDate();
+        $horaireStage=$laFormation->getHoraire();
+        $TitreFormation=$laFormationInfo->getTitre();
         $nbPlace=$laFormationInfo->getNbplace();
         $nbTotalInscrit=$laFormation->getNbinscrit();
         $lInscription= new Inscription;
@@ -27,16 +30,25 @@ class InscriptionController extends Controller
                 if ($laForm->isValid()){
                     $em=$this->getDoctrine()->getEntityManager();
                     $em->persist($lInscription);
+                    $lInscription->getIdFormation()->setNbinscrit(($lInscription->getIdFormation()->getNbinscrit()+$lInscription->getNbinscrit()));
                     $em->flush();
                     $mess='L\'inscription est effectuée.'; 
+                    return $this->render('LamMdlBundle:Formulaire:inscriptionInfo.html.twig',array('TitreFormation'=>$TitreFormation,'dateStage'=>$dateStage,'horaireStage'=>$horaireStage,'mess'=>$mess));
         }
                         }
-        return $this->render('LamMdlBundle:Formulaire:inscriptionInfo.html.twig',array('form'=>$laForm->createView(),'idFormation'=>$idFormation,  'mess'=>$mess));
+        return $this->render('LamMdlBundle:Formulaire:inscriptionInfo.html.twig',array('form'=>$laForm->createView(),'TitreFormation'=>$TitreFormation,'dateStage'=>$dateStage,'horaireStage'=>$horaireStage,'nbPlace'=>$nbPlace, 'nbTotalInscrit'=>$nbTotalInscrit, 'idFormation'=>$idFormation,  'mess'=>$mess));
     }
     public function InscriptionSportIndexAction($idFormation)
     {
+        
         $em = $this->getDoctrine()->getEntityManager();
         $laFormation=$em->getRepository('LamMdlBundle:formation')->find($idFormation);
+        $laFormationSport=$laFormation->getLaformationsport();
+        $dateStage=$laFormation->getDate();
+        $horaireStage=$laFormation->getHoraire();
+        $TitreFormation=$laFormationSport->getTitre();
+        $nbPlace=$laFormationSport->getNbplace();
+        $nbTotalInscrit=$laFormation->getNbinscrit();
         $lInscription= new Inscription;
         $lInscription->setIdFormation($laFormation);
         $laForm=$this->createForm(new inscriptionType(),$lInscription);
@@ -47,10 +59,12 @@ class InscriptionController extends Controller
                 if ($laForm->isValid()){
                     $em=$this->getDoctrine()->getEntityManager();
                     $em->persist($lInscription);
+                    $lInscription->getIdFormation()->setNbinscrit(($lInscription->getIdFormation()->getNbinscrit()+$lInscription->getNbinscrit()));
                     $em->flush();
                     $mess='L\'inscription est effectuée.';
+                    return $this->render('LamMdlBundle:Formulaire:inscriptionSport.html.twig',array('TitreFormation'=>$TitreFormation,'dateStage'=>$dateStage,'horaireStage'=>$horaireStage, 'mess'=>$mess));
         }
         }
-        return $this->render('LamMdlBundle:Formulaire:inscriptionSport.html.twig',array('form'=>$laForm->createView(),'idFormation'=>$idFormation, 'mess'=>$mess));
+        return $this->render('LamMdlBundle:Formulaire:inscriptionSport.html.twig',array('form'=>$laForm->createView(),'TitreFormation'=>$TitreFormation,'dateStage'=>$dateStage,'horaireStage'=>$horaireStage,'nbPlace'=>$nbPlace, 'nbTotalInscrit'=>$nbTotalInscrit, 'idFormation'=>$idFormation, 'mess'=>$mess));
     }
 }
